efdc1c495753e0e40a2ee485657eeaf0ae5bcb81
daothao03 <daothao.gau2003@gmail.com>
2023-09-24 11:25:12 +0700

    Commit 2

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,21 +3,17 @@ const $$ = document.querySelectorAll.bind(document);
 
 const tabs = $$(".tab-items");
 const panes = $$(".magazine");
-const imgs = $$(".img-items");
 
 const tabActive = $(".tab-items.actions");
 
 tabs.forEach((tab, index) => {
     const pane = panes[index];
-    const img = imgs[index];
 
     tab.onclick = function () {
         $(".tab-items.actions").classList.remove("actions");
         $(".magazine.actions").classList.remove("actions");
-        $(".img-items.actions").classList.remove("actions");
 
         this.classList.add("actions");
         pane.classList.add("actions");
-        img.classList.add("actions");
     };
 });
