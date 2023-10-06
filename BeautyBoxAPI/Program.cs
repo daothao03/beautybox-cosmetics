5b65cd7948f3e98604ae7eef64a4cfcf289e2be1
daothao03 <daothao.gau2003@gmail.com>
2023-10-06 22:33:37 +0700

    init

---
+++ BeautyBoxAPI/Program.cs
@@ -0,0 +1,33 @@
+using BeautyBoxAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    string connectString = builder.Configuration.GetConnectionString("bbox")!; //! ??m b?o r?ng 1 chu?i không r?ng
+    options.UseSqlServer(connectString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
